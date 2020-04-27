6fbf191fe6ec1a2dc61c4c0954ed4f18f1b95e92
Kostiantyn Rybnikov <k-bx@k-bx.com>
2020-04-27 10:58:14 +0300

    Unquote a bit

--- src/Main.hs
+++ src/Main.hs
@@ -5,14 +5,14 @@ import qualified RIO.Process
 import qualified RIO.Prelude.Simple
 
 -- put your abs path
-absPythonPath = "/Users/kb/workspace/rio-process-symlink-issue/venv/bin/python"
+absPythonPath = "/home/kb/workspace/rio-process-symlink-issue/venv/bin/python"
 
 main :: IO ()
 main = do
   RIO.Prelude.Simple.runSimpleApp $ do
     (out, err) <- RIO.Process.proc
       absPythonPath
-      ["-c", "\"import semver; print(semver.parse(\\\"1.2.3\\\"))\""]
+      ["-c", "import semver; print(semver.parse(\"1.2.3\"))"]
       $ \pconf -> RIO.Process.readProcess_ pconf
     RIO.liftIO $ print $ "> out: " <> show out
     RIO.liftIO $ print $ "> err: " <> show err
